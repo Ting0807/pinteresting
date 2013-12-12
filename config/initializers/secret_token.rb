ddccf7c802d6520580db92caf95f7c28fcf35c2c
xiaoting wright <haley.xt.wright@gmail.com>
2013-12-12 19:42:10 +0000

    initilise pinteresting project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintest::Application.config.secret_key_base = '7e2b2d54d814472de98b3f5047e6708af382c2b2abf6a5d86892f21011d59d971c7fb70f745a732635fbfee75bd05602f8a597e30b84fb7b12ef4b03d961bd10'
